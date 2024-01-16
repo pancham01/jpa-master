e21bad25dea6828f0d7ace7078ee8bbe18584b84
pancham01 <pancham9557@gmail.com>
2024-01-16 12:55:47 +0530

    update

--- src/main/java/jpa/training/Runner.java
+++ src/main/java/jpa/training/Runner.java
@@ -9,20 +9,26 @@ public class Runner {
 	static StudentRepo stuRepo = new StudentRepo();
 	
 	public static void main(String[] args) {
-		save();
+//		save();
 //		stuRepo.findAndUpdate(1);
 //		stuRepo.findAndDelete(3);
 //		System.out.println("Operation completed");
 //		
-		List<Student> findAll = stuRepo.findAll();
-		findAll.forEach(System.out::println);
+//		List<Student> findAll = stuRepo.findAll();
+//		findAll.forEach(System.out::println);
+		
+//		for (Student student : findAll) {
+//			System.out.println(student);
+//		}
+//		stuRepo.findAndUpdate(1);
+		stuRepo.findAndDelete(1);
 		System.out.println("..........");
 	}
 
 	private static void save() {
-		Student stu1 = new Student("Raghav",2400,24);
+		Student stu1 = new Student("Raghav",2400,54);
 		Student stu2 = new Student("Aditya",2500,25);
-		Student stu3 = new Student("Aman",2600,26);
+		Student stu3 = new Student("Aman",2600,20);
 		stuRepo.persist(stu1);
 		System.out.println("Successully save in db" + stu1);
 		stuRepo.persist(stu2);
